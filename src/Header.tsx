1cb0f96ee62afc071deff1bcd832187654734c55
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-02-14 22:56:13 +0900

    [React] headbar

--- src/Header.tsx
+++ src/Header.tsx
@@ -10,10 +10,33 @@ const Container = styled.div`
 
 const HeaderBar = styled.div`
   height: 10vh;
-  dis
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 `;
 
 const Logo = styled.span``;
+
+const Title = styled.span`
+  padding-left: 8px;
+  font-weight: 700;
+`;
+
+const RightBox = styled.div``;
+
+const Search = styled.span`
+  padding-right: 18px;
+  cursor: pointer;
+`;
+
+const Notice = styled.span`
+  cursor: pointer;
+`;
+
+const Layout = styled.div`
+  width: 100px;
+  background: yellow;
+`;
 
 const Header = () => {
   return (
@@ -22,7 +45,18 @@ const Header = () => {
         <HeaderBar>
           <Logo>
             <i className="fab fa-hive"></i>
+            <Title>Alex Julia</Title>
           </Logo>
+
+          <Layout></Layout>
+          <RightBox>
+            <Search>
+              <i className="fas fa-search"></i>
+            </Search>
+            <Notice>
+              <i className="far fa-bell"></i>
+            </Notice>
+          </RightBox>
         </HeaderBar>
       </Container>
     </>
